8596510386867eada00433a79cf713105457b38c
alexey.anipchenko <alex.anipchenko@gmail.com>
2020-06-03 17:53:19 +0300

    Add translate interface

---
+++ MicrosoftTranslateApp/MicrosoftTranslateApp/Traslators/ITranslator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace MicrosoftTranslateApp.Traslators
+{
+    public interface ITranslator
+    {
+        SortedDictionary<string, string> Languages { get; }
+
+        string Translate(string codeFrom, string codeTo, string textToTranslate);
+    }
+}
