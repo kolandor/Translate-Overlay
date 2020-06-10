950e8c6b4faeb584008d796802f1df6d50fa06ae
alexey.anipchenko <alex.anipchenko@gmail.com>
2020-06-10 04:34:27 +0300

    Add tts interface

---
+++ MicrosoftTranslateApp/MicrosoftTranslateApp/SpeechToText/ISpeechToText.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrosoftTranslateApp.SpeechToText
+{
+    /// <summary>
+    /// Audio recognize service
+    /// </summary>
+    public interface ISpeechToText
+    {
+        /// <summary>
+        /// Recognize audio file
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        string RecognizeFile(string path);
+
+        /// <summary>
+        /// Async recognize audio file
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        Task<string> RecognizeFileAsync(string path);
+
+        /// <summary>
+        /// Recognize audio stream
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        string RecognizeAudioStream(Stream source);
+
+        /// <summary>
+        /// Async recognize audio stream
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        Task<string> RecognizeAudioStreamAsync(Stream source);
+    }
+}
